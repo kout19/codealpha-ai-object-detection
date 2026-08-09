a81ad9aed02062014a513420b46280719b56b9d1
Kefyalew <koutlook19@gmail.com>
2026-08-09 12:04:25 +0300

    Initialize ai-object-detection

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-3xl text-blue-900">
+      <h1>Object Detection</h1>
+    </div>
+      
+</>   
+  )
+}
+  
+
+export default App
